48151954b81ebac1867f04ffafdbd922c09aa50e
parkjm514 <71753415+parkjm514@users.noreply.github.com>
2025-06-20 03:27:35 +0900

    docker 설정 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,30 @@
-import { ProductModule } from './product/product.module';
-import { AccountModule } from './account/account.module';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtMiddleware } from './shared/middleware/jwt.middleware';
-import { SessionMiddleware } from './shared/middleware/session.middleware';
-import { AdministratorModule } from './administrator/administrator.module';
+import { ProductModule } from "./product/product.module";
+import { AccountModule } from "./account/account.module";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtMiddleware } from "./shared/middleware/jwt.middleware";
+import { SessionMiddleware } from "./shared/middleware/session.middleware";
+import { AdministratorModule } from "./administrator/administrator.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: 'envs/.env.dev',
+      envFilePath: "./envs/.env.dev",
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER_NAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        type: "mysql",
+        host: config.get<string>("DB_HOST"),
+        port: config.get<number>("DB_PORT"),
+        username: config.get<string>("DB_USER_NAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        database: config.get<string>("DB_DATABASE"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
         synchronize: true,
       }),
       inject: [ConfigService],
@@ -38,6 +38,6 @@ import { AdministratorModule } from './administrator/administrator.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(JwtMiddleware, SessionMiddleware).forRoutes('*');
+    consumer.apply(JwtMiddleware, SessionMiddleware).forRoutes("*");
   }
 }
